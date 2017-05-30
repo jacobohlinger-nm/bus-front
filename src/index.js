f12b68cced81d44a837591d0ead61df7e7c4d4c9
jacobohlinger-nm <jacobohlinger@northwesternmutual.com>
2017-05-29 20:41:31 -0500

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {render} from 'react-dom';
 import Root from './js/components/Root';
 import registerServiceWorker from './js/registerServiceWorker';
 import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import configureStore from './js/configureStore';
 
 const store = configureStore();
